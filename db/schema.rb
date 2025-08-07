5d9f032a05110fdd49f267e9a61d8979851a7ac1
Reinis Vimba <130878803+reinis-vimba@users.noreply.github.com>
2025-08-07 09:30:20 +0000

    add quiz logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_07_090510) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_07_091403) do
+  create_table "quizzes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
